198b2b2a0766c07ea7c6a1bc605484054239e2e0
dmitrmaster <52205952+dmitrmaster@users.noreply.github.com>
2019-06-26 19:16:58 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
